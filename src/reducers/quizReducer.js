7529b846bb0ed71e5eea4f9242bb10b3b04d1684
Zaheen04 <khanzaheen799@gmail.com>
2024-03-20 21:43:28 +0530

    setting project

---
+++ src/reducers/quizReducer.js
@@ -0,0 +1,16 @@
+// quizReducer.js
+
+const initialState = {
+    // Initial state for quiz
+  };
+  
+  const quizReducer = (state = initialState, action) => {
+    switch (action.type) {
+      // Define cases for quiz actions
+      default:
+        return state;
+    }
+  };
+  
+  export default quizReducer;
+  
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,16 @@
+// userReducer.js
+
+const initialState = {
+    // Initial state for user
+  };
+  
+  const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+      // Define cases for user actions
+      default:
+        return state;
+    }
+  };
+  
+  export default userReducer;
+  
