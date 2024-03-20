6266f9660ad7be855fe6f3a2be3e764e83d04eb0
Zaheen04 <khanzaheen799@gmail.com>
2024-03-20 22:16:40 +0530

    adding components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Quiz from './components/Quiz';
+import Leaderboard from './components/Leaderboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/quiz" component={Quiz} />
+        <Route path="/leaderboard" component={Leaderboard} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
+
